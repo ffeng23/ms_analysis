14a9479243e2438ef37044eb5dc9f7f98d5b345e
Feng Feng <ffeng23@gmail.com>
2023-01-10 15:32:00 -0500

    adding R code to start doing format converting

---
+++ DataProcess.R
@@ -0,0 +1,22 @@
+#R code to process the row data
+#   -- COPYRIGHT by Feng @ Sensei Bio Jan. 2023
+#		--version 1.0
+#---
+# Tasks: 1) read in the data 
+#		 2) preprocess the data
+#		 3) reformat to make the format identifical to: i)MaxQuant output data files;
+#				ii)Msstate compatible input files
+#		 4) write output.
+#		 5) (To do in the future) data analysis: clean up, filter, normalize, 
+#		 		visualize, statistical comparision.
+#---
+#
+
+
+#loading libraries.
+#
+library(here)
+library(dplyr)
+library(tidyr)
+#library(readr)
+
